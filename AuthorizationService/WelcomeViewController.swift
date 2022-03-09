b99be677151b34db0af22a3c484389af43be86f4
EduardKungurtsev <e.kungurtsev@2gis.ru>
2022-03-10 01:09:26 +0700

    Welcome logic

---
+++ AuthorizationService/WelcomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  WelcomeViewController.swift
+//  AuthorizationService
+//
+//  Created by Кунгурцев Эдуард Сергеевич on 09.03.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var nameWelcome: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    }
+    
+    @IBAction func logOutButtonPressed() {
+        dismiss(animated: true)
+    }
+    
+ 
+
+}
