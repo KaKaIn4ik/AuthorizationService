6fa1680b9a955a97d1518ed0dea678fdbe2ba27b
EduardKungurtsev <e.kungurtsev@2gis.ru>
2022-03-09 18:19:50 +0700

    Initial Commit

---
+++ AuthorizationService/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthorizationService
+//
+//  Created by Кунгурцев Эдуард Сергеевич on 09.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
