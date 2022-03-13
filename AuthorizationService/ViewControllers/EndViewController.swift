f2b611630c4c60dcd4acc5a535626fd79da1598a
EduardKungurtsev <e.kungurtsev@2gis.ru>
2022-03-14 03:04:02 +0700

    Update logic endVC

--- AuthorizationService/ViewControllers/EndViewController.swift
+++ AuthorizationService/ViewControllers/EndViewController.swift
@@ -8,4 +8,15 @@
 import UIKit
 
 class EndViewController: UIViewController {
+    @IBOutlet var factsLabel: UILabel!
+    
+    var facts = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        factsLabel.text = " \(facts)!"
+        
+
+    }
 }
